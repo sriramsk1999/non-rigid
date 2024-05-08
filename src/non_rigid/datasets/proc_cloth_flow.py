c67b8c718535c4f101533124765f8d52e4600690
ey-cai <eycai@andrew.cmu.edu>
2024-05-08 19:07:39 -0400

    new dedo experiment + updated logging and visualizations + cloth policy evals + generalized action anchor processing

--- src/non_rigid/datasets/proc_cloth_flow.py
+++ src/non_rigid/datasets/proc_cloth_flow.py
@@ -15,15 +15,24 @@ import torch_geometric.transforms as tgt
 from pathlib import Path
 import os
 
+from non_rigid.utils.pointcloud_utils import downsample_pcd
+
 
 class ProcClothFlowDataset(data.Dataset):
-    def __init__(self, root, type="train"):
+    def __init__(self, root, dataset_cfg, type="train"):
         # This is a toy dataset - no need to normalize or otherwise process point cloud with torch geometric
         super().__init__()
         self.root = root
         self.type = type
         self.dataset_dir = self.root / self.type
         self.num_demos = int(len(os.listdir(self.dataset_dir)))
+        self.dataset_type = "flow"
+
+        self.dataset_cfg = dataset_cfg
+        self.scene = self.dataset_cfg.type_args.scene
+        self.sample_size_action = self.dataset_cfg.sample_size_action
+        self.sample_size_anchor = self.dataset_cfg.sample_size_anchor
+        
     
     def __len__(self):
         return self.num_demos
@@ -31,31 +40,67 @@ class ProcClothFlowDataset(data.Dataset):
     def __getitem__(self, index):
         # load data
         demo = np.load(self.dataset_dir / f"demo_{index}.npz")
-        pc_init = torch.as_tensor(demo["pc_init"]).float()
+        action_pc = torch.as_tensor(demo["action_pc"]).float()
+        action_seg = torch.as_tensor(demo["action_seg"]).int()
+        anchor_pc = torch.as_tensor(demo["anchor_pc"]).float()
+        anchor_seg = torch.as_tensor(demo["anchor_seg"]).int()
+        speed_factor = torch.as_tensor(demo["speed_factor"]).float()
         flow = torch.as_tensor(demo["flow"]).float()
-        seg = torch.as_tensor(demo["seg"]).int()
-        # return pc_init, flow, seg
-        return {
-            "pc_init": pc_init,
-            "flow": flow,
-            "seg": seg,
-        
-        }
+        rot = demo["rot"]
+        trans=demo["trans"]
+
+        # downsampling anchor point cloud
+        anchor_pc, anchor_pc_indices = downsample_pcd(anchor_pc.unsqueeze(0), self.sample_size_anchor, type=self.dataset_cfg.downsample_type)
+        anchor_pc = anchor_pc.squeeze(0)
+        anchor_seg = anchor_seg[anchor_pc_indices.squeeze(0)]
+
+
+        if self.scene:
+            scene_pc = torch.cat([action_pc, anchor_pc], dim=0)
+            scene_seg = torch.cat([action_seg, anchor_seg], dim=0)
+            anchor_flow = torch.zeros_like(anchor_pc)
+            scene_flow = torch.cat([flow, anchor_flow], dim=0)
+            item = {
+                "pc": scene_pc, # Scene points in starting position
+                "pc_action": scene_pc + scene_flow, # Scene points in goal position
+                "seg": scene_seg, 
+                "flow": scene_flow,
+                "speed_factor": speed_factor,
+                "rot": rot,
+                "trans": trans,
+            }
+        else:
+            item = {
+                "pc": action_pc, # Action points in starting position
+                "pc_action": action_pc + flow, # Action points in goal position
+                "pc_anchor": anchor_pc, # Anchor points in goal position
+                "seg": action_seg,
+                "seg_anchor": anchor_seg,
+                "speed_factor": speed_factor,
+                "flow": flow,
+                "rot": rot,
+                "trans": trans,
+            }
+        return item
     
 class ProcClothFlowDataModule(L.LightningDataModule):
-    def __init__(self, root, batch_size, val_batch_size, num_workers):
+    def __init__(self, root, batch_size, val_batch_size, num_workers, dataset_cfg):# type, scene):
         super().__init__()
         self.root = root
         self.batch_size = batch_size
         self.val_batch_size = val_batch_size
         self.num_workers = num_workers
+        self.stage = None
+        self.dataset_cfg = dataset_cfg
 
     def prepare_data(self) -> None:
         pass
 
-    def setup(self, stage: str):
-        self.train_dataset = ProcClothFlowDataset(self.root, "train")
-        self.val_dataset = ProcClothFlowDataset(self.root, "val")
+    def setup(self, stage: str = "train"):
+        self.stage = stage
+        self.train_dataset = ProcClothFlowDataset(self.root, self.dataset_cfg, "train")
+        self.val_dataset = ProcClothFlowDataset(self.root, self.dataset_cfg, "val")
+        self.val_ood_dataset = ProcClothFlowDataset(self.root, self.dataset_cfg, "val_ood")
         # generator = torch.Generator().manual_seed(42)
         # self.train_set, self.val_set = torch.utils.data.random_split(
         #     dataset, [0.9, 0.1], generator=generator
@@ -64,33 +109,21 @@ class ProcClothFlowDataModule(L.LightningDataModule):
         return data.DataLoader(
             self.train_dataset,
             batch_size=self.batch_size,
-            shuffle=True,
+            shuffle=True if self.stage == "train" else False,
             num_workers=self.num_workers,
         )
     
     def val_dataloader(self):
-        return data.DataLoader(
+        val_dataloader = data.DataLoader(
             self.val_dataset,
-            batch_size=self.batch_size,
-            shuffle=True,
+            batch_size=self.val_batch_size,
+            shuffle=False,
             num_workers=self.num_workers,
         )
-    
-
-if __name__ == "__main__":
-    root = Path(os.path.expanduser("~/datasets/nrp/ProcCloth/single_cloth/demos/"))
-    type = "val"
-
-    dataset = ProcClothFlowDataset(root, type)
-
-    points = []
-    segs = []
-
-    for i in range(len(dataset)):
-        pc_init, flow, seg = dataset[i]
-        points.append(pc_init + flow)
-        segs.append(seg * i)
-    points = np.concatenate(points, axis=0)
-    segs = np.concatenate(segs, axis=0)
-    fig = vpl.segmentation_fig(points, segs)
-    fig.show()
+        val_ood_dataloader = data.DataLoader(
+            self.val_ood_dataset,
+            batch_size=self.val_batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+        return val_dataloader, val_ood_dataloader
